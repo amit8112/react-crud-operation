aaf7dce2b1a58190d5b3bde5180581fdc7186da8
amittripathir <amittripathir@gmail.com>
2023-06-20 09:53:33 +0530

    Updates

---
+++ precache-manifest.1f8868be8b3a9a139dc04dd570290065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47130b562882405093ac8b4933bab13c",
+    "url": "/react-crud-operation/index.html"
+  },
+  {
+    "revision": "5fbd706c07dee72e66a0",
+    "url": "/react-crud-operation/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "119dc9d60652f6043e42",
+    "url": "/react-crud-operation/static/css/main.932c2beb.chunk.css"
+  },
+  {
+    "revision": "5fbd706c07dee72e66a0",
+    "url": "/react-crud-operation/static/js/2.29184157.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-crud-operation/static/js/2.29184157.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "119dc9d60652f6043e42",
+    "url": "/react-crud-operation/static/js/main.2707338d.chunk.js"
+  },
+  {
+    "revision": "9b1b85ee32fcb040271e",
+    "url": "/react-crud-operation/static/js/runtime-main.efcd3923.js"
+  }
+]);
